7d05eb249463dd62e06d9c59b702e01d77d841f5
Rupali <rupalichauhan022@gmail.com>
2024-01-06 09:40:09 +0530

    git testing

--- src/Exceptionhandling/TestThrows.java
+++ src/Exceptionhandling/TestThrows.java
@@ -4,6 +4,7 @@ import java.io.FileNotFoundException;
 
 public class TestThrows {
     public static void main(String[] args) {
+        //i added comment for github testing
         UseThrows useThrows=new UseThrows();
         try{
             useThrows.read();
